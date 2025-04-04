60eebb52534063ed1cd20d4285104378438e44b4
kmoreaux <depaninfo54@gmail.com>
2025-04-04 13:57:11 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button'
+import Header from './Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Button variant="primary">Primary</Button>
+      <h1>Hello World</h1>
+    </>
+ )
+}
+
+export default App
